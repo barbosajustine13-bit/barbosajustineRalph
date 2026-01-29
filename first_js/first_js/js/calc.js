4ac9089e9d63096c882c03be6d02524b5fc7f7a1
barbosajustine13-bit <barbosajustine13@gmail.com>
2026-01-29 16:10:46 +0300

    Delete first_js/first_js/js/calc.js

--- first_js/first_js/js/calc.js
+++
@@ -1,9 +0,0 @@
-let display = document.getElementById('display');
-
-function appendNumber(num) {
-    if (display.value === '0' && num !== '.') {
-        display.value = num;
-    } else {
-        display.value += num;
-    }
-}
